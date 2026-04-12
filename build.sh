875b76860dad74af336b78da5f9b97e54f30c5d6
tobiglaser <76131623+tobiglaser@users.noreply.github.com>
2026-04-12 16:25:03 +0200

    Enable GitHub Release Workflow

--- build.sh
+++ build.sh
@@ -1,43 +1,49 @@
-#!/bin/sh
-
-echo "args:"
-echo $#
+#!/bin/bash
 
 if [[ $# -eq 0 ]]
     then
-        echo "Specify Version e.g. 4.2.0"
+        echo 'Specify Version e.g. "4.2.0"'
         exit
     else
         VERSION=$1
         STATUS="testing"
 fi
 
+echo ENV:
+env
+
+rm -rf build/*
 mkdir -p build/plugins/icons
 
-cp -r resources/ build/
-cp resources/icon.png build/plugins/icons/
-cp src/icons/*.png build/plugins/icons/
-cp src/*.py build/plugins/
-cp src/requirements.txt build/plugins/requirements.txt
-cp src/plugin.json build/plugins/plugin.json
+cp -rv resources/ build/
+cp -v resources/icon.png build/plugins/icons/
+cp -v src/icons/*.png build/plugins/icons/
+cp -v src/*.py build/plugins/
+cp -v src/requirements.txt build/plugins/requirements.txt
+cp -v src/plugin.json build/plugins/plugin.json
 
 
-jq  --arg version "$VERSION" \
-    --arg status "$STATUS" \
-    '(.versions[0]) += {"version": "$version", "status": "$status"}' \
+jq  --arg version $VERSION \
+    --arg status $STATUS \
+    '(.versions[0]) += {"version": $version, "status": $status}' \
     metadata.json > build/metadata.json
 
 
-FULLSIZE=$(wc --bytes --total=only build/*)
+FULLSIZE=($(du build/ -bcs))
 
 cd build
-zip KiPNG.zip -r ./*
+zip KiPNG.zip -rv ./* 
 cd -
 
 ZIPSIZE=($(wc --bytes build/KiPNG.zip))
 ZIPSHA=($(sha256sum build/KiPNG.zip))
 
-LINK="https://tobiglaser.de/kicad-tests/KiPNG.zip"
+if [[ $CI ]]
+    then
+        LINK="https://github.com/tobiglaser/KiPNG/releases/download/$VERSION/KiPNG.zip"
+    else
+        LINK="https://tobiglaser.de/kicad-tests/KiPNG.zip"
+fi
 
 jq  --argjson ds $ZIPSIZE \
     --argjson install $FULLSIZE \
@@ -48,3 +54,5 @@ jq  --argjson ds $ZIPSIZE \
     '(.versions[0]) += {"version": $version, "status": $status, "download_size": $ds, "download_sha256": $sha, "install_size": $install, "download_url": $url}' \
     metadata.json > build/metadata.json
 
+echo Metadata:
+cat build/metadata.json
